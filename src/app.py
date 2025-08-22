e3c6eab13b86f730a9ad76ce0cebbf6c876263b7
jun <somang567@naver.com>
2025-08-22 11:14:38 +0900

    기본세팅(2)

--- src/app.py
+++ src/app.py
@@ -1,9 +1,12 @@
+# src/app.py
 from flask import Flask
 from .controller.test import test_blueprint
 
 def create_app():
+    # Flask 애플리케이션 객체 생성
     app = Flask(__name__)
     
     # BLUEPRINT 등록
-    app.register_blueprint(test_blueprint , url_prefix="/api")
+    app.register_blueprint(test_blueprint, url_prefix="/api")
+    
     return app
